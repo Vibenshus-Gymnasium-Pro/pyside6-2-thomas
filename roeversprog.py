4b91880c7203cb21ab2719a6f1c2dc21033a541b
CoolTL <thomasliisborg@gmail.com>
2025-09-02 11:31:04 +0200

    added the translation code

--- roeversprog.py
+++ roeversprog.py
@@ -1,12 +1,45 @@
 # [[file:README.org::*Udvikling af logik][Udvikling af logik:1]]
 # Dette er et modul til oversættelse mellem almindelige sprog og røversprog
 
+vowel_l = "aeiouyæøå"
+consonants = "bcdfghjklmnpqrstvwz"
+
 def oversaet_til_roeversprog(inputtekst):
-    outputtekst = "Denne funktion skal kunne oversætte til røversprog."
+    outputtekst = ""
+
+    
+
+    
+    
+
+    for i in inputtekst:
+        if i.lower() in vowel_l:
+            outputtekst += i
+        elif i.lower() in consonants:
+            outputtekst += i + "o" + i
+
+
     return outputtekst
 
 
 def oversaet_fra_roeversprog_til_andet_sprog(inputtekst):
-    outputtekst = "Her skal røversproget fjernes og almindeligt sprog skal returneres.\nGiv gerne fejlmeddelelser, hvis røversproget ikke er korrekt."
+    outputtekst = ""
+    
+    skip = 0
+
+    for i in (inputtekst):
+        if skip > 0:
+            skip -= 1
+            
+        else:
+            if i.lower() in consonants:
+                outputtekst += i
+                skip = 2
+            elif i.lower() in vowel_l:
+                outputtekst += i
+
     return outputtekst
-# Udvikling af logik:1 ends here
+# Udvikling af logik:1 ends here 
+
+print(oversaet_til_roeversprog("tAEst"))
+print(oversaet_fra_roeversprog_til_andet_sprog("totAEsostot"))
